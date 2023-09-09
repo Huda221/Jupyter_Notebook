ca0169d839bf50252977dc20190bd88bf9e7a47a
Huda Zahid <142784857+Huda221@users.noreply.github.com>
2023-09-09 17:18:07 +0500

    Add files via upload

---
+++ Chapter1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Chapter 1
+
+# In[3]:
+
+
+x=1 
+print(x)
+x=x+1
+print(x)
+
+
+# In[4]:
+
+
+# Sequential steps
+x=2
+x=x+2
+print(x)
+
+
+# In[5]:
+
+
+# conditional steps
+x=5            
+if x<10:
+    print("smaller")
+if x>20:
+    print("Bigger")
+    
+print("Done")    
+
+
+# In[6]:
+
+
+# repeated steps
+y=5
+while y>0:
+    print(y)
+    y=y-1
+print("Reverse counting")    
+
+
+# In[ ]:
+
+
+
+
